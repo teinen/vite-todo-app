a7b58398de4e3f4d23bd76ce28edc25a15807959
Akira Kanno <teinen.github@gmail.com>
2021-04-06 23:23:19 +0900

    Add very simple todos

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1 class="title">Todo app with Vite / Vue 3</h1>
+  <div class="container">
+    <h2 class="subtitle">All Todo</h2>
+    <TodoList :items="todoItems" />
+
+    <hr>
+
+    <h2 class="subtitle">Completed Todo</h2>
+    <TodoList :items="completedTodoItems" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, defineComponent, onMounted, watch, computed } from 'vue'
+import { TodoItem } from './types'
+import TodoList from './components/TodoList.vue'
 
 export default defineComponent({
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    TodoList
+  },
+
+  setup() {
+    const todoItems = ref([] as TodoItem[])
+    const initTodoItems = (): void => {
+      todoItems.value = [
+        {
+          id: 1,
+          title: 'Study Vite',
+          isCompleted: false
+        },
+        {
+          id: 2,
+          title: 'Study Vue 3',
+          isCompleted: true
+        },
+        {
+          id: 3,
+          title: 'Create Todo app',
+          isCompleted: false
+        },
+      ]
+    }
+
+    const completedTodoItems = computed(() => {
+      return todoItems.value.filter(item => {
+        return item.isCompleted
+      })
+    })
+
+    onMounted(initTodoItems)
+
+    return {
+      todoItems,
+      completedTodoItems
+    }
+  },
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.title {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 16px;
+  background-color: #eceded;
+  border: 1px solid #eceded;
+  border-radius: 10px;
+}
+
+.subtitle {
+
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="item in items" :key="item.id">
+    {{ item.id }}: {{ item.title }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { TodoItem } from '../types'
+
+export default defineComponent({
+  props: {
+    items: Array as PropType<TodoItem[]>
+  },
+
+  setup() {
+
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
